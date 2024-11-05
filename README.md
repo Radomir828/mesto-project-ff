9529eb1ec31607bf021cd5e63ccdf496b2fe43d6
Radomir828 <radsobic@yandex.com>
2024-11-05 20:57:26 +0300

    feat: добавление и удаление карточек

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Проектная работа Mesto
 
+https://github.com/Radomir828/mesto-project-ff.git
--- scripts/index.js
+++ scripts/index.js
@@ -7,3 +7,42 @@
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
+
+const cardsList = document.querySelector(".places__list");
+const addButton = document.querySelector(".profile__add-button");
+
+function addCard(cardData, deleteCard) {
+  const cardTemplate = document.querySelector("#card-template").content;
+
+  const cardElement = cardTemplate.querySelector(".card").cloneNode(true);
+  const cardElementImage = cardElement.querySelector(".card__image");
+  const cardElementTitle = cardElement.querySelector(".card__title");
+
+  cardElementImage.src = cardData.link;
+  cardElementTitle.textContent = cardData.name;
+
+  const cardElementDeleteButton = cardElement.querySelector(
+    ".card__delete-button"
+  );
+  cardElementDeleteButton.addEventListener("click", () => {
+    deleteCard(cardElement);
+  });
+
+  return cardElement;
+}
+
+function deleteCard(cardElement) {
+  cardElement.remove();
+}
+
+// добавляем все карточки на страницу
+initialCards.forEach((cardData) => {
+  const cardElement = addCard(cardData, deleteCard);
+
+  // для второй карточки добавляем фильтр, чтобы была видна инонка удаления карточки
+  if (cardData.name === "Челябинская область") {
+    let cardImage = cardElement.querySelector(".card__image");
+    cardImage.classList.add("filter-brightness");
+  }
+  cardsList.append(cardElement);
+});
